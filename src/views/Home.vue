836c37d7b216776eff3d454b5f4f99d986031191
lb66 <807242867@qq.com>
2020-12-18 15:25:36 +0800

    初始化，引入依赖

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    xx
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
+
+<style >
+.home{
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  background: #eee;
+}
+</style>
+
+
