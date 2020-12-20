a93ae279ac639a3e373d2e483dd2e60dc9c0ef12
lb66 <807242867@qq.com>
2020-12-20 16:58:56 +0800

    saleView的header

--- src/components/SalesView.vue
+++ src/components/SalesView.vue
@@ -1,15 +1,80 @@
 <template>
-  <div>
-
+  <div class="sales-view">
+    <el-card shadow="hover">
+      <div slot="header">
+        <div class="menu-wrapper">
+          <el-menu
+            :default-active="activeIndex"
+            class="el-menu"
+            mode="horizontal"
+            @select="handleSelect"
+          >
+            <el-menu-item index="1">销售额</el-menu-item>
+            <el-menu-item index="2">访问量</el-menu-item>
+          </el-menu>
+          <div class="menu-right">
+            <el-radio-group
+              v-model="radio"
+              size="small"
+              :style="{ marginRight: '20px' }"
+            >
+              <el-radio-button label="今日"></el-radio-button>
+              <el-radio-button label="本周"></el-radio-button>
+              <el-radio-button label="本月"></el-radio-button>
+              <el-radio-button label="今年"></el-radio-button>
+            </el-radio-group>
+            <el-date-picker
+              v-model="value"
+              type="daterange"
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              size="small"
+            >
+            </el-date-picker>
+          </div>
+        </div>
+      </div>
+      <div v-for="o in 4" :key="o" class="text item">
+        {{ "列表内容 " + o }}
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {
+  data() {
+    return {
+      activeIndex: "1",
+      radio: "今年",
+      value: "",
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      this.activeIndex = key;
+      console.log(key);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+.sales-view {
+  margin-top: 20px;
+  .menu-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .el-menu .el-menu-item {
+      height: 60px;
+      line-height: 60px;
+      margin-right: 30px;
+    }
+    .el-menu.el-menu--horizontal {
+    border-bottom: none
+    }
+  }
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
-import { Button,Card,Col,Row } from 'element-ui'
+import { Button,Card,Col,Row,Menu,MenuItem,RadioGroup,RadioButton,DatePicker } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Card)
 Vue.use(Col)
 Vue.use(Row)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(RadioGroup)
+Vue.use(RadioButton)
+Vue.use(DatePicker)
