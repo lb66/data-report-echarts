d0307058fcbe9f83d526c719568bddb7675fc197
lb66 <807242867@qq.com>
2020-12-21 14:41:51 +0800

    完成salesView架构

--- src/components/SalesView.vue
+++ src/components/SalesView.vue
@@ -35,8 +35,18 @@
           </div>
         </div>
       </div>
-      <div v-for="o in 4" :key="o" class="text item">
-        {{ "列表内容 " + o }}
+      <div class="sales-chart-wrapper">
+        <v-chart :options="chartOptions" />
+        <div class="sales-list">
+          <div class="list-title">排行榜</div>
+          <div class="list-item" v-for="item in rankData" :key="item.no">
+            <div :class="['list-item-no', +item.no <= 3 ? 'top-no' : '']">
+              {{ item.no }}
+            </div>
+            <div class="list-item-name">{{ item.name }}</div>
+            <div class="list-item-money">{{ item.money }}</div>
+          </div>
+        </div>
       </div>
     </el-card>
   </div>
@@ -49,6 +59,59 @@ export default {
       activeIndex: "1",
       radio: "今年",
       value: "",
+      chartOptions:{
+        title:{
+          text:'年度销售额',
+          textStyle:{
+            fontSize:12,
+            color:'#666'
+          }
+        },
+        grid: { bottom: 50, top: 50, right: 40, left: 40 },
+        xAxis: {
+          type:'category',
+          data:['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
+          axisTick:{
+            alignWithLabel:true,
+            lineStyle:{
+              color:'#999'
+            }
+          },
+          axisLine:{
+            lineStyle:{
+              color:'#999'
+            }
+          },
+          axisLabel:{
+            color:'#333'
+          },
+        },
+        yAxis: {
+          axisLine:{show:false},
+          axisTick:{show:false},
+          splitLine:{
+            lineStyle:{
+              type:'dotted',
+              color:'#eee'
+            }
+          }
+        },
+        series: {
+          type:'bar',
+          barWidth:'35%',
+          data:[200,250,300,200,250,300,200,250,300,200,250,300]
+        },
+        color:['#3398DB']
+      },
+      rankData: [
+        { no: 1, name: "肯德基", money: "211335" },
+        { no: 2, name: "肯德基", money: "211335" },
+        { no: 3, name: "肯德基", money: "211335" },
+        { no: 4, name: "肯德基", money: "211335" },
+        { no: 5, name: "肯德基", money: "211335" },
+        { no: 6, name: "肯德基", money: "211335" },
+        { no: 7, name: "肯德基", money: "211335" },
+      ],
     };
   },
   methods: {
@@ -73,7 +136,52 @@ export default {
       margin-right: 30px;
     }
     .el-menu.el-menu--horizontal {
-    border-bottom: none
+      border-bottom: none;
+    }
+  }
+  .sales-chart-wrapper {
+    display: flex;
+    height: 270px;
+    .echarts {
+      flex: 0 0 70%;
+      width: 70%;
+      height: 100%;
+    }
+    .sales-list {
+      flex: 1;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      .list-title {
+        font-size: 12px;
+        color: #666;
+        font-weight: 500;
+      }
+      .list-item {
+        font-size: 12px;
+        display: flex;
+        margin: 18px 0;
+        .list-item-money{
+          margin-left: auto;
+        }
+        .list-item-no{
+          width: 18px;
+          height: 18px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          &.top-no{
+            background: #000;
+            border-radius: 50%;
+            color: #fff;
+            font-weight: 500;
+          }
+
+        }
+        .list-item-name{
+          margin-left: 16px;
+        }
+      }
     }
   }
 }
